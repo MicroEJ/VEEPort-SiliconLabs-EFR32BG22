6aa51a87b4dd8aea355e2984fae3cc5ae35b6445
MicroEJ GitHub Delivery <delivery@microej.com>
2021-05-03 12:06:27 +0000

    Version 0.2.1

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,7 +7,7 @@ buildWithAnt{
 	LABEL = "microej_sdk&&iar"					// The label where build must be run. Default value is : public. Set it if yours is different.
 	
 	//ANT_VERSION = ""				// The version of ant by Microej. Default value is : MMM MicroEJ 5.1.0. Set it if yours is different.
-	ANT_PROPERTIES = "-Declipse.home=c:/jenkinsTools/common/microej/eclipse/microej-sdk-20.10/win64"				// The properties run with ant. Default value is already set. Add specific properties if necessary. As ('-DnameOfProperties=valueofProperty').
+	ANT_PROPERTIES = "-Declipse.home=c:/jenkinsTools/common/microej/eclipse/microej-sdk-20.10/win64 -Dskip.publish=false"				// The properties run with ant. Default value is already set. Add specific properties if necessary. As ('-DnameOfProperties=valueofProperty').
 	//ANT_VERBOSE = ""				// Default value is : false. Set true if you want the verbose mode. 
 
 	//PATTERN_JUNIT = ''				// The pattern Junit for test report. This default value is : '**/target~/test/xml/**/*test-report.xml, **/target~/test/xml/**/*Test.xml'. Set it if you want to use different one.
